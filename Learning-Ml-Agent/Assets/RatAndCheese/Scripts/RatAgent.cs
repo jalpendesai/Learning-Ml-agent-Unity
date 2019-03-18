918adf38b10d872d626a93522d082f602397c0b8
jalpendesai <djalpen24@gmail.com>
2019-03-18 14:56:24 -0400

    nnn

---
+++ Learning-Ml-Agent/Assets/RatAndCheese/Scripts/RatAgent.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+public class RatAgent : Agent
+{
+    Rigidbody rbody;
+
+    public float speed = 10.0f;
+    public float turnSpeed = 5.0f;
+
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody>();
+    }
+
+    public override void AgentReset()
+    {
+      this.transform.position = new Vector3(0f,0.5f,0f);
+    }
+
+    public override void AgentAction(float[] vectorAction, string textAction)
+    {
+        float absDir = 0f;
+        Vector3 dir = Vector3.zero;
+        Vector3 rotateDir = Vector3.zero;
+
+        if (brain.brainParameters.vectorActionSpaceType == SpaceType.continuous)
+        {
+            absDir = Mathf.Clamp(vectorAction[0], -1f, 1f);
+            dir = transform.position * absDir;
+
+            // rotateDir = transform.up * Mathf.Clamp(vectorAction[1], -1f, 1f);
+        }
+        // else
+        // {
+        //     switch ((int)(vectorAction[0]))
+        //     {
+        //         case 1:
+        //             dir = transform.forward;
+        //             break;
+        //         case 2:
+        //             rotateDir = -transform.up;
+        //             break;
+        //         case 3:
+        //             rotateDir = transform.up;
+        //             break;
+        //     }
+        // }
+        rbody.AddForce(dir * speed, ForceMode.VelocityChange);
+        Vector3 localVel = transform.InverseTransformDirection(rbody.velocity);
+        if (localVel.z < 0)
+        {
+            // TODO: Penalize if player goes backward
+        }
+        transform.Rotate(rotateDir, Time.fixedDeltaTime * turnSpeed);
+
+        if(this.transform.position.y < 0.3){
+          Done();
+      }
+    }
+}
